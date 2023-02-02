34a7e1756dec759e32d7e05ba0c405de8afb98ab
Innovanito <khg05311@naver.com>
2023-02-02 14:11:23 +0900

    230202 predict function successfully return the dic value

--- api/main.py
+++ api/main.py
@@ -3,28 +3,44 @@ import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
-# import tensorflow as tf
+import tensorflow as tf
 
 app = FastAPI()
 
+MODEL = tf.keras.models.load_model("../saved_models/2")
 
-@app.get("/")
-async def ping():
-    return 'hey'
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
+
     return image
 
 
-@app.get("/predict")
+@app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, axis=0)
 
-    return
+    prediction = MODEL.predict(img_batch)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='losthost', port=8000)
+    perdicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+
+    confidence = np.max(prediction[0])
+
+    return {
+        'class': perdicted_class,
+        'confidence': float(confidence),
+    }
+
+
+@app.get("/")
+async def root():
+    return {"message": 'Hi 😎'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
